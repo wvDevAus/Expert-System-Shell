56c070b31e291bbcb8f9dbb27b7b6ca18d11c231
William Vlahos <wxavierv@gmail.com>
2020-10-29 15:25:24 +0800

    - Updated the DynamicENum to be compatible with the JSON loading library. - Fixed an issue with a missing file listing in the src/CMakeLists.txt.

--- src/databases/facts/FactTypes.cpp
+++ src/databases/facts/FactTypes.cpp
@@ -7,7 +7,12 @@ namespace expert_system {
         enum_ = DynamicEnum(ordered_names);
     }
 
+    VariantFact::VariantFact() : type_(FactType::kUnknown) {}
+
     VariantFact::VariantFact(FactType type) {
+        // Store the hint for the generated type
+        type_ = type;
+
         // Generate and store a specific Fact specialization
         switch (type) {
             case FactType::kBoolFact:
@@ -22,10 +27,19 @@ namespace expert_system {
             case FactType::kEnumFact:
                 fact_ = EnumFact();
                 break;
+            default:
+                fact_ = std::monostate();
+                type_ = FactType::kUnknown;
+                break;
         }
-
-        // Store the hint for the generated type
-        type_ = type;
     }
+
+    /*void to_json(nlohmann::json& json_sys, const VariantFact& target) {
+
+    }*/
+
+    /*void from_json(const nlohmann::json& json_sys, VariantFact& target) {
+
+    }*/
 
 } // namespace expert_system
