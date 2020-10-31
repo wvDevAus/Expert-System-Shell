7ad10543075d294d34ebfc18f13110d68e8e9cd6
William Vlahos <wxavierv@gmail.com>
2020-10-31 22:24:44 +0800

    - Added template Assignment struct.

---
+++ src/knowledge/rules/Assignment.hpp
@@ -0,0 +1,148 @@
+#pragma once
+
+#include <optional>
+#include <string>
+#include <variant>
+
+#include "knowledge/facts/Database.hpp"
+#include "knowledge/facts/Facts.hpp"
+#include "knowledge/rules/Conditions.hpp"
+
+namespace expert_system::knowledge::rules {
+
+        /**
+         * @brief Template Assignment.
+         * @tparam T The raw type used in the Assignment.
+         */
+    template <class T>
+    struct Assignment {
+            /// Default constructor.
+        Assignment() = default;
+
+            /// The identifying name of the target Fact.
+        std::string fact_;
+
+            /// The value to assign to the target Fact.
+        T value_;
+
+            /**
+             * @brief Attempts to perform the Assignment
+             * @param [in] database The Fact Database to operate on.
+             * @param [in] source The name of the Rule that triggered this Assignment, or std::nullopt to indicate the user.
+             * @param [in] confidence_factor The confidence factor from the triggered Condition.
+             * @return True if the target Fact was assigned a session value, False otherwise.
+             */
+        bool Assign(facts::FactDatabase& database,
+                    std::optional<std::reference_wrapper<std::string>> source,
+                    utility::Confidence& confidence_factor) {
+            // Non-specialized types are not supported
+            return false;
+        }
+    };
+
+        /// Boolean specialization overload of templated Assignment::Assign.
+    template<>
+    bool Assignment<bool>::Assign(facts::FactDatabase& database,
+                                  std::optional<std::reference_wrapper<std::string>> source,
+                                  utility::Confidence& confidence_factor) {
+        // Confirm the identified Fact actually exists
+        auto optional_fact = database.Find(fact_);
+        if (!optional_fact.has_value()) {
+            // Stop and indicate failure
+            return false;
+        }
+
+        // Gather the raw Fact by reference
+        auto& variant_fact = optional_fact.value().get();
+        if (variant_fact.type_ != utility::ExpertSystemTypes::kBool) {
+            // Catch type mismatch and indicate failure
+            return false;
+        }
+
+        // Assign the Fact the session value
+        auto& raw_fact = std::get<facts::BoolFact>(variant_fact.fact_);
+        raw_fact.SetValue(value_, confidence_factor, source);
+        return true;
+    }
+
+        /// Integer specialization overload of templated Assignment::Assign.
+    template<>
+    bool Assignment<int>::Assign(facts::FactDatabase& database,
+                                 std::optional<std::reference_wrapper<std::string>> source,
+                                 utility::Confidence& confidence_factor) {
+        // Confirm the identified Fact actually exists
+        auto optional_fact = database.Find(fact_);
+        if (!optional_fact.has_value()) {
+            // Stop and indicate failure
+            return false;
+        }
+
+        // Gather the raw Fact by reference
+        auto& variant_fact = optional_fact.value().get();
+        if (variant_fact.type_ != utility::ExpertSystemTypes::kInt) {
+            // Catch type mismatch and indicate failure
+            return false;
+        }
+
+        // Assign the Fact the session value
+        auto& raw_fact = std::get<facts::IntFact>(variant_fact.fact_);
+        raw_fact.SetValue(value_, confidence_factor, source);
+        return true;
+    }
+
+        /// Float specialization overload of templated Assignment::Assign.
+    template<>
+    bool Assignment<float>::Assign(facts::FactDatabase& database,
+                                   std::optional<std::reference_wrapper<std::string>> source,
+                                   utility::Confidence& confidence_factor) {
+        // Confirm the identified Fact actually exists
+        auto optional_fact = database.Find(fact_);
+        if (!optional_fact.has_value()) {
+            // Stop and indicate failure
+            return false;
+        }
+
+        // Gather the raw Fact by reference
+        auto& variant_fact = optional_fact.value().get();
+        if (variant_fact.type_ != utility::ExpertSystemTypes::kFloat) {
+            // Catch type mismatch and indicate failure
+            return false;
+        }
+
+        // Assign the Fact the session value
+        auto& raw_fact = std::get<facts::FloatFact>(variant_fact.fact_);
+        raw_fact.SetValue(value_, confidence_factor, source);
+        return true;
+    }
+
+        /// Enumeration specialization overload of templated Assignment::Assign.
+    template<>
+    bool Assignment<std::string>::Assign(facts::FactDatabase& database,
+                                         std::optional<std::reference_wrapper<std::string>> source,
+                                         utility::Confidence& confidence_factor) {
+        // Confirm the identified Fact actually exists
+        auto optional_fact = database.Find(fact_);
+        if (!optional_fact.has_value()) {
+            // Stop and indicate failure
+            return false;
+        }
+
+        // Gather the raw Fact by reference
+        auto& variant_fact = optional_fact.value().get();
+        if (variant_fact.type_ != utility::ExpertSystemTypes::kEnum) {
+            // Catch type mismatch and indicate failure
+            return false;
+        }
+
+        // Assign the Fact the session value
+        auto& raw_fact = std::get<facts::EnumFact>(variant_fact.fact_);
+        auto enum_conversion = raw_fact.enum_.At(value_);
+        if (enum_conversion == std::nullopt) {
+            // Catch the enum value not existing
+            return false;
+        }
+        raw_fact.fact_.SetValue(enum_conversion.value(), confidence_factor, source);
+        return true;
+    }
+
+} // namespace expert_system::knowledge::rules
